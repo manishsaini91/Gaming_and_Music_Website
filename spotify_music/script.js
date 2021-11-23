efc3ff31661e0db5dd041d68f6802616bd45e0c9
manishsaini91 <manishsaini6528@gmail.com>
2021-11-23 20:13:07 +0530

    variables created

--- spotify_music/script.js
+++ spotify_music/script.js
@@ -1,3 +1,10 @@
+let songIndex=0;
+let audioElement=new Audio('1.mp3');
+let masterPlay=document.getElementById('masterPlay');
+let myProgressBar=document.getElementById('myProgressBar');
+
+
+
 let songs=[{songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
 {songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
 {songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
@@ -8,4 +15,8 @@ let songs=[{songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.j
 {songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
 {songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"},
 {songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"}]
-let audioElement=new Audio('1.mp3');
+
+
+
+
+document
