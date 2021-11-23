47f376c063418230ba7ad7d9e60255919976b24c
manishsaini91 <manishsaini6528@gmail.com>
2021-11-23 20:47:20 +0530

    playing different songs

--- spotify_music/script.js
+++ spotify_music/script.js
@@ -2,6 +2,9 @@ let songIndex=0;
 let audioElement=new Audio('1.mp3');
 let masterPlay=document.getElementById('masterPlay');
 let myProgressBar=document.getElementById('myProgressBar');
+let gif = document.getElementById('gif');
+let masterSongName = document.getElementById('masterSongName');
+let songItems = Array.from(document.getElementsByClassName('songItem'));
 
 
 
@@ -17,6 +20,25 @@ let songs=[{songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.j
 {songName :"Salam-e-Ishq",filePath:"song/1.mp3",coverPath:"covers/1.jpg"}]
 
 
+songItems.forEach((element, i)=>{ 
+  element.getElementsByTagName("img")[0].src = songs[i].coverPath; 
+  element.getElementsByClassName("songName")[0].innerText = songs[i].songName; 
+})
 
 
-document
+
+masterPlay.addEventListener('click', ()=>{
+  if(audioElement.paused || audioElement.currentTime<=0){
+      audioElement.play();
+      masterPlay.classList.remove('fa-play-circle');
+      masterPlay.classList.add('fa-pause-circle');
+      gif.style.opacity = 1;
+  }
+  else{
+      audioElement.pause();
+      masterPlay.classList.remove('fa-pause-circle');
+      masterPlay.classList.add('fa-play-circle');
+      gif.style.opacity = 0;
+  }
+})
+
