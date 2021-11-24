778ddb3bc5518d3899f091e0da43ec71ae0c65f9
Lakhan <lakhan.nishad_cs19@gla.ac.in>
2021-11-24 21:27:12 +0530

    add firebase configuration

---
+++ Auth/index.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyAcWTJcc5MLFsjh0jGd7ri9cDb3GPKK7Wc",
+    authDomain: "gamingandmusicwebsite.firebaseapp.com",
+    projectId: "gamingandmusicwebsite",
+    storageBucket: "gamingandmusicwebsite.appspot.com",
+    messagingSenderId: "479615859987",
+    appId: "1:479615859987:web:8aef5ab2898bb5414c2ee6"
+  };
+  firebase.initializeApp(firebaseConfig);
+  firebase.analytics();
